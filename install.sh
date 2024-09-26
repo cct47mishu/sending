91a244f48ad706f73b5572084e295286dd2841cc
cct47mishu <123601430+cct47mishu@users.noreply.github.com>
2024-09-26 21:42:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-#!/bin/bash; sudo apt update -y && sudo apt install wkhtmltopdf -y && sudo apt install python3-pip -y && python3 -m pip install -r req.txt && nohup python3 app.py > app.log 2>&1 & echo "Flask app started and running in the background. Logs can be found in app.log."
+#!/bin/bash; 
+sudo apt update -y && sudo apt install wkhtmltopdf -y && sudo apt install python3-pip -y && python3 -m pip install -r req.txt && nohup python3 app.py > app.log 2>&1 & echo "Flask app started and running in the background. Logs can be found in app.log."
